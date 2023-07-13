ae01d42b67aab2e31ec6151392a2113de93e7c5a
vivinkv6 <vivinkv0@gmail.com>
2023-07-14 04:15:28 +0530

    fix deploy error

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,8 +9,6 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import { Link } from "react-router-dom";
 
 
 function NavBar() {
